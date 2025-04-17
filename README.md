dd1ebb943b53c2c18656cd75bf7a9cd36a74a9d9
Marcel Dellmann <admin@secure-bits.org>
2025-04-17 11:50:45 +0200

    Adds description and build options
    Adds a description of the tool to the README.
    
    Improves setup script by adding comments and build options.

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Python GUI-Program for analyse log-files
 
-
+**With this log analysis tool, you can analyze and filter log files (even large ones) and export the results using a graphical user interface.**
--- setup.py
+++ setup.py
@@ -1,18 +1,20 @@
 import sys
 from cx_Freeze import setup, Executable
 
+# Optionen für den Build-Prozess
 build_exe_options = {
-   „excludes“: [„tkinter“, „unittest“],
-   „zip_include_packages“: [„encodings“, „PySide6“],
+   "excludes": ["tkinter", "unittest"],  # Module, die explizit ausgeschlossen werden sollen
+   "zip_include_packages": ["encodings", "PySide6"], # Pakete, die in das Zip-Archiv aufgenommen werden sollen
 }
 
-# base=“Win32GUI“ sollte nur mit der Windows GUI App verwendet werden
-base = „Win32GUI“ if sys.platform == „win32“ else None
+# Basis für die ausführbare Datei bestimmen
+# "Win32GUI" wird für Windows GUI-Anwendungen verwendet, um das Konsolenfenster zu unterdrücken
+base = "Win32GUI" if sys.platform == "win32" else None
 
 setup(
-   name=“Log-Analyse“,
-   version=“0.1″,
-   description=“Log-Analyser for Logs“,
-   options={„build_exe“: build_exe_options},
-   executables=[Executable(„Loganalyse.py“, base=base)],
+   name="Log-Analyse",        # Name der Anwendung
+   version="0.1",             # Version der Anwendung
+   description="Log-Analyser for Logs", # Beschreibung
+   options={"build_exe": build_exe_options}, # Übergabe der Build-Optionen
+   executables=[Executable("Loganalyse.py", base=base)], # Definition der zu erstellenden ausführbaren Datei
 )
